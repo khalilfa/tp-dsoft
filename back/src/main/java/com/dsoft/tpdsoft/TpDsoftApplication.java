56ca28860c0885d87e47eb6ff4bb3c68d0b92836
Facundo <facundokhalil@gmail.com>
2019-08-21 14:33:34 -0300

    Se creo un proyecto base con springboot

---
+++ back/src/main/java/com/dsoft/tpdsoft/TpDsoftApplication.java
@@ -0,0 +1,13 @@
+package com.dsoft.tpdsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpDsoftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpDsoftApplication.class, args);
+    }
+
+}
