13cb8ea5034723789d14f49cd8511a5e5a08324d
jose <msanchez.jose.94@gmail.com>
2019-09-18 18:22:12 -0300

    add Menu class with respective validations

---
+++ back/src/main/java/com/dsoft/tpdsoft/model/Menu.java
@@ -0,0 +1,37 @@
+package com.dsoft.tpdsoft.model;
+
+import java.util.ArrayList;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Size;
+
+import org.springframework.data.annotation.Id;
+
+
+@Entity
+public class Menu {
+	
+	@Id @GeneratedValue
+	private Integer id;
+	
+	@Size(min=4, max=30)
+	private String name;
+	
+	@Size(min=20, max=40)
+	private String description;
+	
+	private ArrayList<Category> categories; // TODO valideta at least one category
+	
+	private DeliveryInfo deliveryInfo;
+	
+	private Float price;
+	
+	@Max(20)
+	private Integer maxSalesPerDay;
+	
+	private String effectiveDate; // obviously is a todo
+	
+	
+}
