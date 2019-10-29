ee3895f359a2fe2735bbf9649be56dbb294e6458
jose <msanchez.jose.94@gmail.com>
2019-10-29 19:39:32 -0300

    basic MenuBrowser template

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
 import Maps from './components/Maps';
+import MenuBrowser from './components/MenuBrowser';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Route exact path="/" component={Login} />
       <Route exact path="/register" component={Register} />
       <Route exact path="/maps" component={Maps} />
+      <Route exact path="/browseMenu" component={MenuBrowser} />
     </Router>
   );
 }
---
+++ frontend/src/components/MenuBrowser.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class MenuBrowser extends React.Component {
+    render(){
+        return (
+            <div>
+                <h1>Buscador de Menues</h1>
+                {/* Options to filter menues */}
+                <section>
+        
+
+                </section>
+                {/* Given menu list after aplicate filter */}
+                <section >
+                
+                </section>
+            
+            </div>
+            
+        );
+
+    }
+
+}
+
+export default MenuBrowser;
