5bbc147dce36f86f795817f1d9b0e57194114b68
Facundo <facundokhalil@gmail.com>
2019-11-05 16:36:40 -0300

    Se agrego i18n para cambiar idioma

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -30,6 +30,7 @@ class Register extends React.Component {
   }
 
   render() {
+    const { t } = this.props;
     return (
       <div className="app">
         <div className="app-title row col-ms-4 offset-md-4">
@@ -38,87 +39,87 @@ class Register extends React.Component {
         </div>
         <div className="login row col-md-4 offset-md-4">
           <h1 className="login-title col-md-9 aling-self-left">
-            Registro
+            {t('Register')}
           </h1>
           <form className="form" onSubmit={this.handleSubmit}>
             <label className="register-label row">
-              <span className="register-text col">Nombre: </span>
+              <span className="register-text col">{'Name'}: </span>
               <input
                 type="text"
                 value={this.state.name}
                 name="name"
-                placeholder="Ingresar nombre"
+                placeholder={t('Enter name')}
                 onChange={this.handleInputChange}
               />
             </label>
 
             <label className="register-label row">
-              <span className="register-text col">Apellido: </span>
+              <span className="register-text col">{t('Lastname')}: </span>
               <input
                 type="text"
                 value={this.state.lastname}
                 name="lastname"
-                placeholder="Ingresar apellido"
+                placeholder={t('Enter lastname')}
                 onChange={this.handleInputChange}
               />
             </label>
 
             <label className="register-label row">
-              <span className="register-text col">Email: </span>
+              <span className="register-text col">{t('Email')}: </span>
               <input
                 type="email"
                 value={this.state.email}
                 name="email"
-                placeholder="Ingresar email"
+                placeholder={t('Enter email')}
                 onChange={this.handleInputChange}
               />
             </label>
 
             <label className="register-label row">
-              <span className="register-text col">Contraseña: </span>
+              <span className="register-text col">{t('Password')}: </span>
               <input
                 type="password"
                 value={this.state.password}
                 name="password"
-                placeholder="Ingresar contraseña"
+                placeholder={t('Enter password')}
                 onChange={this.handleInputChange}
               />
             </label>
 
             <label className="register-label row">
-              <span className="register-text col">Repetir: </span>
+              <span className="register-text col">{t('Repeat')}: </span>
               <input
                 type="password"
                 value={this.state.repeatPassword}
                 name="repeatPassword"
-                placeholder="Repitar la contraseña"
+                placeholder={t('Repeat password')}
                 onChange={this.handleInputChange}
               />
             </label>
 
             <label className="register-label row">
-              <span className="register-text col">Telefono: </span>
+              <span className="register-text col">{t('Phone')}: </span>
               <input
                 type="text"
                 name="phone"
                 value={this.state.phone}
-                placeholder="Ingresar telefono"
+                placeholder={t('Enter phone number')}
                 onChange={this.handleInputChange}
               />
             </label>
 
             <label className="register-label row">
-              <span className="register-text col">Direccion: </span>
+              <span className="register-text col">{t('Address')}: </span>
               <input
                 type="text"
                 value={this.state.address}
                 name="address"
-                placeholder="Ingresar direccion"
+                placeholder={t('Enter address')}
                 onChange={this.handleInputChange}
               />
             </label>
 
-            <button type="submit" className="btn btn-primary">Registrar</button>
+            <button type="submit" className="btn btn-primary">{t('Register')}</button>
           </form>
         </div>
       </div>
