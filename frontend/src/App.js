dc86bb2474c87dc42625f7ab156a56c084b9b182
ColoJose <msanchez.jose.94@gmail.com>
2019-12-04 00:32:21 -0300

    Issue 45 improve existing screen designs (#49)
    * grid created for client screen
    
    * search menus client component created
    
    * add user rol select to GeneralNavbar
    
    * add i18n to GeneralNavbar
    
    * add basic Router to App
    
    * modify client screen - partial implementation
    
    * fix to pass codacy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,53 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route,Switch } from 'react-router-dom';
 import { withTranslation } from 'react-i18next';
-import Index from './components/Index';
 import Register from './components/Register';
 import Maps from './components/Maps';
 import Provider from './components/Provider';
-import MenuBrowser from './components/MenuBrowser';
 import Bill from './components/Bill';
 import CreateProvider from './components/CreateProvider';
-import LanguageButton from './components/LanguageButton';
+import GeneralNavbar from './components/GeneralNavbar';
 import Client from './components/Client';
+import Index from './components/Index';
+import HomePageClient from './components/HomePageClient';
+import './css/indexGrid.css';
 
 function App({ t }) {
+
+//  const [user,setUser] = React.useState(null); -- to set onLogged // ToDo
+//  const [rol,setRol]   = React.useState(null); -- to set onLogged // ToDo
+
+  function handleLog(event){
+    event.preventDefault();
+    // to do
+  }
   return (
     <div>
-      <LanguageButton />
       <Router>
-        <Route exact path="/" render={(props) => <Index {...props} t={t} />} />
-        <Route exact path="/client/:idClient" render={(props) => <Client {...props} t={t} />} />
-        <Route exact path="/register" render={(props) => <Register {...props} t={t} />} />
-        <Route exact path="/maps" render={(props) => <Maps {...props} t={t} />} />
-        <Route exact path="/provider/:idProvider" render={(props) => <Provider {...props} t={t} />} />
-        <Route exact path="/bill" render={(props) => <Bill {...props} t={t} />} />
-        <Route exact path="/browseMenu" render={(props) => <MenuBrowser {...props} t={t} />} />
-        <Route exact path="/createProvider" render={(props) => <CreateProvider {...props} t={t} />} />
+        <div>
+          
+          <GeneralNavbar t={t} />
+          <Switch>
+            <Route 
+              exact 
+              path="/" 
+              render={(props) =>
+                                <Index {...props}
+                                t={t} 
+                                onLogged={ (event) => handleLog(event) }  />} />
+            />
+            <Route exact path="/client/:idClient" render={(props) => <Client {...props} t={t} />} />
+            <Route exact path="/register" render={(props) => <Register {...props} t={t} />} />
+            <Route exact path="/maps" render={(props) => <Maps {...props} t={t} />} />
+            <Route exact path="/provider/:idProvider" render={(props) => <Provider {...props} t={t} />} />
+            <Route exact path="/bill" render={(props) => <Bill {...props} t={t} />} />
+            <Route exact path="/createProvider" render={(props) => <CreateProvider {...props} t={t} />} />
+            <Route exact path="/home" render={(props) => <HomePageClient {...props} t={t} />} />
+          </Switch>    
+        </div>
       </Router>
     </div>
   );
 }
-
 
 export default withTranslation()(App);
---
+++ frontend/src/components/FilterSide.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const FilterSide= ({t}) => (
+    <h1>{t("Sort menus by")}</h1>
+);
+
+export default FilterSide;
