d7b3ae74f1836130662917f2f8c797f378434029
Facundo Khalil <facundokhalil@gmail.com>
2019-12-09 14:55:28 -0300

    Se agrego la busqueda de menus por categoria y el filtro por nombre o descripcion paginables

--- back/src/main/java/com/dsoft/tpdsoft/repositories/MenuRepository.java
+++ back/src/main/java/com/dsoft/tpdsoft/repositories/MenuRepository.java
@@ -2,12 +2,13 @@ package com.dsoft.tpdsoft.repositories;
 
 import com.dsoft.tpdsoft.model.Category;
 import com.dsoft.tpdsoft.model.Menu;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface MenuRepository extends PagingAndSortingRepository<Menu, Integer> {
-    List<Menu> findAllByCategories(Category category);
+    Page<Menu> findAllByCategories(Category category, Pageable pageable);
+    Page<Menu> findByNameContainingOrDescriptionContaining(String name, String description, Pageable pageable);
 }
