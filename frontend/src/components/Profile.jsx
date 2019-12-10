65d6bfc51a85cb82a2c2467c7331b07572b90541
Facundo Khalil <facundokhalil@gmail.com>
2019-12-10 10:56:09 -0300

    Se protegieron todas las rutas para que necesiten autenticacion

---
+++ frontend/src/components/Profile.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useAuth0 } from '../react-auth0-spa';
+
+const Profile = () => {
+  const { loading, user } = useAuth0();
+
+  if (loading || !user) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <img src={user.picture} alt="Profile" />
+
+      <h2>{user.name}</h2>
+      <p>{user.email}</p>
+      <code>{JSON.stringify(user, null, 2)}</code>
+    </div>
+  );
+};
+
+export default Profile;
